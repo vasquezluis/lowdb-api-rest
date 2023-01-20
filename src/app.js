6a27c975b5d2146012365085cabb3cec033a5daf
LuiiVasquez <luivasque95@gmail.com>
2023-01-20 09:51:19 -0600

    deleting swagger options

--- src/app.js
+++ src/app.js
@@ -4,27 +4,11 @@ import swaggerUI from "swagger-ui-express";
 import swaggerJSDoc from "swagger-jsdoc";
 import tasksRoutes from "./routes/tasks.routes.js";
 
+import swaggerOptionsFile from "./swagger.json" assert { type: "json" };
+
 const app = express();
 
-// swagger settings
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Tasks API",
-      version: "1.0.0",
-      description: "A simple express library API",
-    },
-    servers: [
-      {
-        url: "http://localhost:3000",
-      },
-    ],
-  },
-  apis: ["./src/routes/*.js"],
-};
-
-const specs = swaggerJSDoc(options);
+const specs = swaggerJSDoc(swaggerOptionsFile);
 
 // middlewares
 app.use(morgan("dev"));
